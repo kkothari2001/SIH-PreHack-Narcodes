229eef81258fb1580bd86b4417481bebd24aa098
Asavari Ambavane <ambavaneasavari@gmail.com>
2022-03-25 12:32:21 +0530

    whychooseus

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -2,6 +2,9 @@ import { Grid } from "@mui/material";
 import React from "react";
 import "../css/Landing.css";
 import img1 from "../assets/LandingPage1.PNG";
+import study1 from "../assets/study1.jpg";
+import study2 from "../assets/study2.jpg";
+import study3 from "../assets/study3.jpg";
 
 const Landing = () => {
   return (
@@ -9,7 +12,14 @@ const Landing = () => {
       <div style={{ maxWidth: "1200px", margin: "auto", padding: "2rem" }}>
         <Grid container>
           <Grid item xs={12} sm={12} md={6}>
-            <div style={{height: "80%", display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+            <div
+              style={{
+                height: "80%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
               <div>
                 <h1 className="title">
                   Tumble, <br /> Lorem Ipsum
@@ -31,6 +41,49 @@ const Landing = () => {
             ></img>
           </Grid>
         </Grid>
+      </div>
+      <div className="why-choose-us">
+        <h1 style={{ textAlign: "center" }}>Why Choose Us?</h1>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          p={3}
+        >
+          <Grid item xs={12} sm={6} md={4}>
+            <div>
+              <img src={study1} alt=""></img>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum
+              </p>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div>
+              <img src={study2} alt=""></img>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum
+              </p>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div>
+              <img src={study3} alt=""></img>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <div className="join-us">
+            Join Us!
+        </div>
       </div>
     </div>
   );
