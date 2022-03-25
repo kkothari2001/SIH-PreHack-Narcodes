05283e94a805969e7e96c22270c686876a7c0fd3
Jatin Salve <jasalve_b19@el.vjti.ac.in>
2022-03-25 15:08:15 +0530

    Image holder and Footer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Landing from './Components/Landing';
-import Navbar from './Components/Navbar';
+import "./App.css";
+import Landing from "./Components/Landing";
+import Navbar from "./Components/Navbar";
+import Tutor from "./Components/Tutor";
+import FooterPage from "./Components/FooterPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Landing />
+      <Tutor />
+      <FooterPage />
     </div>
   );
 }
